0cddbeff0bb0e6a45b34a8c89d67d09e2f826001
juthaporn <jutha.1995@gmail.com>
2021-11-04 16:08:30 +0700

    set db

---
+++ app.js
@@ -0,0 +1,16 @@
+const express  = require('express');
+const db = require("./util/database");
+const app = express();
+
+const adminChopraduRoutes = require('./routes/chopradu');
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+app.use("/admin",adminChopraduRoutes);
+
+app.listen(3000);   
