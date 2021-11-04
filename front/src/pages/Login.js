1f166c5e4e40259831d2bb3c6ee544938ab23920
juthaporn <jutha.1995@gmail.com>
2021-11-04 15:59:50 +0700

    add folder

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -3,6 +3,56 @@ import axios from 'axios';
 
 class Login extends React.Component{
     
+    // (function ($) {
+    //     "use strict";
+
+    //     var input = $('.validate-input .input100');
+    
+    //     $('.validate-form').on('submit',function(){
+    //         var check = true;
+    
+    //         for(var i=0; i<input.length; i++) {
+    //             if(validate(input[i]) == false){
+    //                 showValidate(input[i]);
+    //                 check=false;
+    //             }
+    //         }
+    
+    //         return check;
+    //     });
+        
+    //     $('.validate-form .input100').each(function(){
+    //         $(this).focus(function(){
+    //            hideValidate(this);
+    //         });
+    //     });
+    
+    //     function validate (input) {
+    //         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+    //             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    //                 return false;
+    //             }
+    //         }
+    //         else {
+    //             if($(input).val().trim() == ''){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    
+    //     function showValidate(input) {
+    //         var thisAlert = $(input).parent();
+    
+    //         $(thisAlert).addClass('alert-validate');
+    //     }
+    
+    //     function hideValidate(input) {
+    //         var thisAlert = $(input).parent();
+    
+    //         $(thisAlert).removeClass('alert-validate');
+    //     }
+
+    // })(jQuery);
 
     render(){
         return(
@@ -11,9 +61,9 @@ class Login extends React.Component{
                     <div class="container-login100">
                         <div class="wrap-login100 p-t-50 p-b-90">
                             <form class="login100-form validate-form flex-sb flex-w">
-                                <span class="login100-form-title p-b-51">
+                                {/* <span class="login100-form-title p-b-51">
                                     Login
-                                </span>
+                                </span> */}
 
                                 
                                 <div class="wrap-input100 validate-input m-b-16" data-validate = "Username is required">
@@ -27,7 +77,7 @@ class Login extends React.Component{
                                     <span class="focus-input100"></span>
                                 </div>
                                 
-                                <div class="flex-sb-m w-full p-t-3 p-b-24">
+                                {/* <div class="flex-sb-m w-full p-t-3 p-b-24">
                                     <div class="contact100-form-checkbox">
                                         <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me"/>
                                         <label class="label-checkbox100" for="ckb1">
@@ -40,14 +90,20 @@ class Login extends React.Component{
                                             Forgot?
                                         </a>
                                     </div>
-                                </div>
+                                </div> */}
 
-                                <div class="container-login100-form-btn m-t-17">
-                                    <button class="login100-form-btn">
-                                        Login
-                                    </button>
+                                <div class="flex-sb-m w-full p-t-3 p-b-24">
+                                    <div class="container-login100-form-btn m-t-17">
+                                        <button class="login100-form-btn">
+                                            เข้าสู่ระบบ
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <a href="#" class="txt1">
+                                            ลงทะเบียน
+                                        </a>
+                                    </div>
                                 </div>
-
                             </form>
                         </div>
                     </div>
